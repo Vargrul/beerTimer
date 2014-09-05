191eb4e5e05f165d3b9b72da8a629ccff79939e9
Vargrul <kladef11@student.aau.dk>
2014-09-05 15:39:07 +0200

    First commit

---
+++ Assets/Scripts/DataHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DataHandler : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var ardScript = GetComponent<ArduinoDataReceiver>();
+		var guiScript = GetComponent<GuiHandler>();
+
+		List<string> incommingData = ardScript.receivedData;
+
+		if(incommingData.Count != 0){
+
+			//checking for a number else some kind of text
+			if((int)incommingData[0][0] > 47 && (int)incommingData[0][0] < 58){
+
+				Debug.Log ("it was a number");
+				guiScript.h = Convert.ToInt32(incommingData[0]);
+				guiScript.m = Convert.ToInt32(incommingData[1]);
+				guiScript.s = Convert.ToInt32(incommingData[2]);
+				guiScript.ms = Convert.ToInt32(incommingData[3]);
+
+				guiScript.newTime = true;
+
+				incommingData.Clear();
+			}else{
+
+				Debug.Log ("It was some text");
+
+				switch (incommingData[0]){
+					//Start a GUI Clock
+					case "start":
+						guiScript.running = true;
+					break;
+
+					//Stop a GUI Clock
+					case "stop":
+						guiScript.running = false;
+					break;
+				}
+			}
+		}
+	
+	}
+}
---
+++ Assets/Scripts/GuiHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiHandler : MonoBehaviour {
+
+	public GUIText timer;
+	public GUITexture background;
+	public bool running,newTime;
+	public int h,m,s,ms;
+
+	// Use this for initialization
+	void Start () {
+		timer.text = "00:00:00,00";
+		setGuiBackground();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Updates time when new precise time is passed
+		if(newTime)	setGuiTimer();
+		setGuiBackground();
+
+	}
+
+	void setGuiTimer(){
+		string tempTimerText;
+		
+		if(h < 10) tempTimerText = "0" + h.ToString() + ":"; else tempTimerText = h.ToString() + ":";
+		if(m < 10) tempTimerText += "0" + m.ToString() + ":"; else tempTimerText += m.ToString() + ":";
+		if(s < 10) tempTimerText += "0" + s.ToString() + ","; else tempTimerText += s.ToString() + ",";
+		if(ms < 10) tempTimerText += "0" + ms.ToString(); else tempTimerText += ms.ToString();
+		
+		timer.color = new Color(0.0f,1.0f,0.0f);
+		timer.text = tempTimerText;
+	}
+
+	void setGuiBackground(){
+		
+		//background.pixelInset = (float) Screen.height;
+	}
+}
