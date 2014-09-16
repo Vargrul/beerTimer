322376b5e505f8dce981094a059f0a2f55bd6419
Vargrul <kladef11@student.aau.dk>
2014-09-16 17:10:42 +0200

    Made UI updates and cleaned old GUI code

--- Assets/Scripts/GuiHandler.cs
+++ Assets/Scripts/GuiHandler.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GuiHandler : MonoBehaviour {
 
-	public GUIText timer;
-
-	public GUITexture background;
 	private int oldScrW, oldScrH;
 
 	public bool updateTimeStamp,running,newTime;
@@ -13,35 +11,30 @@ public class GuiHandler : MonoBehaviour {
 	public int h,m,s,ms;
 	private float startTime;
 
+
+    //UI Elements
+    public Text timerText;
+    public Button addUser;
+
+
     // Use this for initialization
 	void Start () {
 		updateTimeStamp = true;
-		timer.text = "00:00:00,00";
-		setGuiBackground();
+        //timerText = GameObject.FindGameObjectWithTag("timer").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
-
 	}
 
     void OnGUI() {
         //Updates time when new precise time is passed
-        setGuiBackground();
 		setGuiTimer();
-
-        guiButtons();
     }
 
-    void guiButtons() {
-
-        //Add User Button
-        if (GUI.Button(new Rect(10, 10, 100, 50), "Add User")) {
-
-        }
-
+    public void testClick() {
+        Debug.Log("clicked");
     }
 
 	void setGuiTimer(){
@@ -61,34 +54,21 @@ public class GuiHandler : MonoBehaviour {
 			if(s < 10) tempTimerText += "0" + s.ToString() + ","; else tempTimerText += s.ToString() + ",";
 			if(ms < 10) tempTimerText += "0" + ms.ToString(); else tempTimerText += ms.ToString();
 
-			timer.color = new Color(1.0f,1.0f,0.0f);
-			timer.text = tempTimerText;
+			timerText.color = new Color(1.0f,1.0f,0.0f);
+            timerText.text = tempTimerText;
 		}
 		if(newTime){
 			if(h < 10) tempTimerText = "0" + h.ToString() + ":"; else tempTimerText = h.ToString() + ":";
 			if(m < 10) tempTimerText += "0" + m.ToString() + ":"; else tempTimerText += m.ToString() + ":";
 			if(s < 10) tempTimerText += "0" + s.ToString() + ","; else tempTimerText += s.ToString() + ",";
 			if(ms < 10) tempTimerText += "0" + ms.ToString(); else tempTimerText += ms.ToString();
-			
-			timer.color = new Color(0.0f,1.0f,0.0f);
-			timer.text = tempTimerText;
+
+            timerText.color = new Color(0.0f,1.0f,0.0f);
+            timerText.text = tempTimerText;
 
 			newTime = false;
 			running = false;
 			updateTimeStamp = true;
-		}
-	}
-
-	void setGuiBackground(){
-		if(oldScrH != Screen.height || oldScrW != Screen.width){
-			float sX,sY,sZ;
-			sX = ((float)(Screen.width/16) / (float)(Screen.height/9) > 1)?1.0f:(float)(Screen.height/9 / (float)(Screen.width/16));
-			sY = ((float) (Screen.width/16) / (float) (Screen.height/9) > 1)?(float)(Screen.width/16) / (float)(Screen.height/9):1.0f;
-			sZ = 1.0f;
-			
-			background.transform.localScale = new Vector3(sX,sY,sZ);
-			oldScrH = Screen.height;
-			oldScrW = Screen.width;
 		}
 	}
 }
